760e7bcc110ab998619b3ce2435c748fc13c9bdc
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-07-25 10:29:33 +0530

    change bg and added a new podcast

--- src/components/common/Podcasts/PodcastsCard/index.js
+++ src/components/common/Podcasts/PodcastsCard/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './style.css'
 import { Link } from 'react-router-dom';
+import { FaPlay } from 'react-icons/fa6';
 
 const PodcastsCard = ({id, title,displayImage}) => {
   return (
    <Link style={{textDecoration:'none'}} to={`/podcast/${id}`}>
     <div className='podcast_Card'>
      <img className='displayImgPodcast' src={displayImage} alt={title}/>
-     <p className='title_podcast'>{title}</p>
+      <div style={{display:'flex', justifyContent:'space-between',alignItems:'center'}}>
+      <p className='title_podcast'>{title}</p>
+     <span className='playIcon'><FaPlay/></span>
+      </div>
     </div>
    </Link>
   );
