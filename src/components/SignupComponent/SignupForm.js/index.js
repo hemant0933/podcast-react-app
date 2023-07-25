a2b932bd3314d71117a18c1c4bd13c9ca4cb8ffb
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-07-25 20:14:29 +0530

    added edit feature in profile page

--- src/components/SignupComponent/SignupForm.js/index.js
+++ src/components/SignupComponent/SignupForm.js/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import InputComponent from "../../common/input";
 import Button from "../../common/Button";
 
-import { auth, db } from "../../../firebase";
+import { auth, db, storage } from "../../../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 import { doc, setDoc } from "firebase/firestore";
@@ -10,12 +10,15 @@ import { useDispatch } from "react-redux";
 import { setUser } from "../../../slice/userSlice";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import FileInput from "../../common/input/FileInput";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const SignupForm = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
@@ -24,47 +27,57 @@ const SignupForm = () => {
   const handleSignUp = async () => {
     setLoading(true);
     console.log("Handling Signup...");
-    if (password === confirmPassword && password.length >= 6) {
+    if (password === confirmPassword && password.length >= 6 && avatar) {
       try {
+        // upload file -->  get downloadable links
+        const avatarRef = ref(
+          storage,
+          `profile/${auth.currentUser.uid}/${Date.now()}`
+        )
+        await uploadBytes(avatarRef, avatar);
+        // console.log( uploaded);  
+        const avatarUrl = await getDownloadURL(avatarRef);
         // Creating a new user
         const useCredential = await createUserWithEmailAndPassword(
           auth,
           email,
-          password
+          password,
+          avatar
         );
         const user = useCredential.user;
-        console.log("User", user);
+        // console.log("User", user);
 
         //Saving user details to firebase
         await setDoc(doc(db, "users", user.uid), {
           name: fullName,
           email: user.email,
           uid: user.uid,
+          avatar:avatarUrl,
         });
-
         //saving data in the redux, call the redux action
         dispatch(
           setUser({
             name: fullName,
             email: user.email,
             uid: user.uid,
+            avatar: avatarUrl,
           })
         );
         setLoading(false);
         navigate("/profile");
       } catch (err) {
-        setLoading(false);
-        console.error("Error", err);
-        toast.error(err.message, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        });
+          setLoading(false);
+          console.error("Error", err);
+          toast.error(err.message, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
       }
     } else {
       setLoading(false);
@@ -93,6 +106,10 @@ const SignupForm = () => {
       }
       //throw an error
     }
+  };
+
+  const handleProfileImage = (file) =>{
+    setAvatar(file)
   };
 
   return (
@@ -125,6 +142,12 @@ const SignupForm = () => {
         placeholder="Confirm Password"
         required={true}
       />
+      <FileInput
+        accept={"image/*"}
+        id="Avatar-id"
+        fileHandleFunc={handleProfileImage}
+        text={"Upload Profile Photo"}
+     />
       <Button
         text={loading ? "Loading..." : "Signup"}
         onClick={handleSignUp}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/common/Header";
 import Button from "../components/common/Button";
 import { signOut } from "firebase/auth";
 import { toast } from "react-toastify";
-import { auth } from "../firebase";
+import { auth, db, storage } from "../firebase";
 import Loader from "../components/common/Loader";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import FileInput from "../components/common/input/FileInput";
+import InputComponent from "../components/common/input";
+import { setUser } from "../slice/userSlice";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Profile = () => {
+  const [avatar, setAvatar] = useState("");
+  const [Isedit, setIsEdit] = useState(false);
+  const [newEmail, setnewEmail] = useState("");
+  const [newAvatar, setnewAvatar] = useState("");
+  const [newName, setnewName] = useState("");
+
   const user = useSelector((state) => state.user.user);
-  
-  if (!user) {
-    return <Loader />;
-  }
-  
+  const dispatch = useDispatch();
+
+
   const handleLogout = () => {
     signOut(auth)
       .then(() => {
@@ -24,13 +33,107 @@ const Profile = () => {
       });
   };
 
+  const handlNewProfileImage = (e) => {
+    setnewAvatar(e.target.files[0]);
+  };
+  const handleEdit = () => {
+    setIsEdit(true);
+    setnewName(user.name);
+    setnewEmail(user.email);
+  };
+
+  const handleSave = async () => {
+    // Upload new profile picture to firbase storage
+    let newAvatarUrl = avatar;
+    if (newAvatar) {
+      const avatarRef = ref(
+        storage,
+        `profile/${auth.currentUser.uid}/${Date.now()}`
+      );
+      await uploadBytes(avatarRef, newAvatar);
+      newAvatarUrl = await getDownloadURL(avatarRef);
+    }
+
+    //Update user Data in firestore
+
+    const userDoc = doc(db, "users", auth.currentUser.uid);
+    await updateDoc(userDoc, {
+      name: newName,
+      email: newEmail,
+      avatar: newAvatarUrl,
+    });
+    // update local state
+    dispatch(
+      setUser({
+        name: newName,
+        email: newEmail,
+        uid: user.uid,
+        avatar: newAvatarUrl,
+      })
+    );
+    setAvatar(newAvatarUrl);
+    setIsEdit(false);
+  };
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userDoc = doc(db, "users", auth.currentUser.uid);
+      const userData = await getDoc(userDoc);
+      if (userData.exists()) {
+        setAvatar(userData.data().avatar);
+      }
+    };
+    getUserData();
+  });
+
+  if (!user) {
+    return <Loader />;
+  }
+  console.log(user);
+
+  if (Isedit) {
+    return (
+      <div>
+        <Header />
+
+        <div className="wrapper">
+          <h1>Update User Data</h1>
+          <InputComponent
+            state={newName}
+            setState={setnewName}
+            type="text"
+            placeholder="Full Name"
+            required={true}
+          />
+          <InputComponent
+            state={newEmail}
+            setState={setnewEmail}
+            type="email"
+            placeholder="Email"
+            required={true}
+          />
+          {/* Add input for profile picture */}
+          <FileInput
+            accept={"image/*"}
+            id="Avatar-id"
+            fileHandleFunc={handlNewProfileImage}
+            text={"Upload Profile Photo"}
+          />
+          <Button text={"Save"} onClick={handleSave} />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
       <div className="wrapper">
-        <h1>{user.name}</h1>
+        <img src={avatar} className="avatar" alt="profile" />
+        <h1 style={{ textTransform: "capitalize" }}>{user.name}</h1>
         <h1>{user.email}</h1>
-        <h1>{user.uid}</h1>
+        {/* <h1>{user.uid}</h1> */}
+        <Button text={"Edit"} onClick={handleEdit} />
         <Button text={"Logout"} onClick={handleLogout} />
       </div>
     </div>
