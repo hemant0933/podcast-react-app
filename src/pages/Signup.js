7d12d5e26a3b09503c6e14e276081e3823a72e5f
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-09-20 21:52:54 +0530

    changed ui of login and sign up page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import Header from '../components/common/Header/index';
+// import Header from '../components/common/Header/index';
 import SignupForm from '../components/SignupComponent/SignupForm.js';
 import LoginFrom from '../components/SignupComponent/LoginFrom';
+import Lottie from "react-lottie-player";
+import signImage from "../Assets/signup_lottie.json";
 
 const Signup = () => {
   
@@ -9,15 +11,30 @@ const Signup = () => {
 
   return (
     <div className='outer_wrapper'>
-     <Header/>
+     {/* <Header/> */}
      <div className='input_Wrapper'>
-     
-      {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
+     <div className='box1'>
+     {!flag ? <h1>Signup</h1> : <h1>Login</h1>}
       {!flag ? <SignupForm/> : <LoginFrom/>}
       {!flag ? <p style={{cursor:'pointer'}} onClick={() => setFlag(!flag)}>Click here if you already have an Account.Login.</p> 
         : 
         <p style={{cursor:'pointer'}} onClick={() => setFlag(!flag)}>Don't have an account? Click here to Signup.</p>}
       
+     </div>
+     <div className='box2'>
+       <div className='svg'>
+        <div className='text'>
+          <h1>Podcast</h1>
+        </div>
+       <Lottie
+          play
+          loop
+          mode="normal"
+          animationData={signImage}
+          className="w-full h-full "
+        />
+       </div>
+     </div>
      </div>
     </div>
   ); 
